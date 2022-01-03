a84434a54adcbe06cb5787b05aa153197a5955d3
ctdarius <spacefintech@spacefintechs-iMac.local>
2022-01-03 16:14:27 +0700

    change server.js

--- server.js
+++ server.js
@@ -8,15 +8,8 @@ const bodyParser = require("body-parser");
 const path = require('path')
 
 const app = express();
-app.use(bodyParser.json({ limit: "500mb", extended: true }));
-app.use(
-	bodyParser.urlencoded({
-		limit: "500mb",
-		extended: true,
-		parameterLimit: 500000,
-	})
-);
 
+app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 8080;
 // settings cors
@@ -25,17 +18,7 @@ app.use(cors({
 	methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
 	allowHeaders: ["Content-Type", "multipart/form-data"],
 }));
-
-// settings socketIO
-const server = http.createServer(app);
-const io = socketIO(server);
-
-// setup socketIO
-io.on("connection", (socket) => console.log("new client connected"));
-
-
 // Khai báo static file
-
 app.use('/static', express.static(path.join(__dirname, './src/public')))
 
 //set route 
@@ -43,6 +26,6 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
   })
 // setting port
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`http://localhost:3000`);
 });
