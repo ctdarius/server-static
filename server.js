e47cc16e054a132c9d15efbf74ac58ec751ae162
ctdarius <spacefintech@spacefintechs-iMac.local>
2022-01-03 16:48:38 +0700

    server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors({
 // Khai báo static file
 app.use('/static', express.static(path.join(__dirname, './src/public')))
 
-//set route 
+// //set route 
 app.get('/', (req, res) => {
     res.send('Hello World!')
   })
